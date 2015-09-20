f0e8300ebe1a7b65692ec03c03644ebbef58e6a9
Agustin Gianni <agustingianni@gmail.com>
2015-09-20 13:51:12 +0200

    Added new target to build a simple disassembler.

---
+++ src/tools/disassembler.cpp
@@ -0,0 +1,15 @@
+/*
+ * dis.cpp
+ *
+ *  Created on: Sep 20, 2015
+ *      Author: anon
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	cout << "Welcome to disassembler hurr" << endl;
+	return 0;
+}
