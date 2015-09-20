d86ea5e67e3dfb01496697f94d2b05202cb0e74e
Agustin Gianni <agustingianni@gmail.com>
2015-09-20 14:01:54 +0200

    Added sample code to the simple disassembler.

--- src/tools/disassembler.cpp
+++ src/tools/disassembler.cpp
@@ -5,11 +5,20 @@
  *      Author: anon
  */
 
+#include "arm/ARMDisassembler.h"
+
+#include <memory>
 #include <iostream>
 
 using namespace std;
+using namespace Disassembler;
 
 int main(int argc, char **argv) {
-	cout << "Welcome to disassembler hurr" << endl;
+	ARMDisassembler dis;
+	uint32_t opcode = 0xe92d4ff0;
+	shared_ptr<ARMInstruction> ins = dis.disassemble(opcode, ARMMode_Thumb);
+	cout << "Disassembled instruction: " << (void *) opcode << " -> "
+			<< ins->toString() << endl;
+
 	return 0;
 }
