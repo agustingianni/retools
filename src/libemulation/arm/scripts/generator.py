86ce4b7724d510936d1081bee6c1cd1d3d01622f
Agustin Gianni <agustingianni@gmail.com>
2015-10-16 22:50:57 +0200

    Saving progress on the emulation side of the generation.

--- src/libemulation/arm/scripts/generator.py
+++ src/libemulation/arm/scripts/generator.py
@@ -1,8 +1,11 @@
 import os
 import re
-import sys
+import json
 import logging
 import argparse
+
+import ARMv7Parser
+import ARVv7OperationSpec
 
 DEBUG = False
 
@@ -12,12 +15,13 @@ def instruction_decoder_name(ins_name):
     ins_name = ins_name.rstrip("_")
     return "interpret_" + ins_name.lower()
 
-def create_interpreter(interpreter_name_h, interpreter_name_cpp):
+def create_interpreter(interpreter_name_h, interpreter_name_cpp, symbols_file):
     """
     Create ARMInterpreter.h and ARMInterpreter.cpp.
-    """
-    import ARMv7Parser
-    from ARVv7OperationSpec import instructions
+    """    
+    # Load symbols file.
+    with open(symbols_file, "r") as fd:
+        symbols = json.load(fd)
 
     with open(interpreter_name_h, "w") as fd:
         header = ""
@@ -26,9 +30,9 @@ def create_interpreter(interpreter_name_h, interpreter_name_cpp):
         header += "    ARMInterpreter() {};\n\n"
         
         fd.write(header)
-        for instruction in instructions:
+        for instruction in ARVv7OperationSpec.instructions:
             ins_name = instruction["name"]
-            fd.write("    bool %s(ARMContext &ctxt);\n" % instruction_decoder_name(ins_name))
+            fd.write("    bool %s(ARMContext &ctx, const ARMInstruction &ins);\n" % instruction_decoder_name(ins_name))
         
         fd.write("};\n")
     
@@ -39,11 +43,11 @@ def create_interpreter(interpreter_name_h, interpreter_name_cpp):
         
         fd.write(header)
         i = 0
-        for instruction in instructions:
+        for instruction in ARVv7OperationSpec.instructions:
             ins_name = instruction["name"]
             logging.info("Doing instruction '%s'" % ins_name)
 
-            fd.write("bool ARMInterpreter::%s(ARMContext &ctxt) {\n" % instruction_decoder_name(ins_name))
+            fd.write("bool ARMInterpreter::%s(ARMContext &ctx, const ARMInstruction &ins) {\n" % instruction_decoder_name(ins_name))
             
             ins_operation = instruction["operation"]
 
@@ -51,7 +55,8 @@ def create_interpreter(interpreter_name_h, interpreter_name_cpp):
             ins_operation = os.linesep.join([s for s in ins_operation.splitlines() if not re.match(r'^\s*$', s)])
             
             try:
-                ret = ARMv7Parser.program.parseString(ins_operation, parseAll=True)
+                program_ast = ARMv7Parser.program.parseString(ins_operation, parseAll=True)
+                            
             except Exception, e:
                 logging.error("-" * 80)
                 logging.error(ins_name)
@@ -80,14 +85,109 @@ def create_interpreter(interpreter_name_h, interpreter_name_cpp):
 
                 logging.error("-" * 80)
                 logging.error(e)
-                logging.error("# ", i, " of ", len(instructions), " to go ", len(instructions) - i)
+                logging.error("# ", i, " of ", len(instructions), " to go ", len(ARVv7OperationSpec.instructions) - i)
                 return False
 
-            i += 1
-            
+            # Set the types for implicit things in the pseudocode.
+            known_types = []
+
+            # Set the types for known expressions.
+            known_types.append({"name" : "ARMExpandImm",                     "type" : ("int", 32)})
+            known_types.append({"name" : "ARMExpandImm_C",                   "type" : ("list", 2)})
+            known_types.append({"name" : "AddWithCarry",                     "type" : ("list", 3)})
+            known_types.append({"name" : "AdvSIMDExpandImm",                 "type" : ("int", 64)})
+            known_types.append({"name" : "Align",                            "type" : ("int", 32)})
+            known_types.append({"name" : "BigEndian",                        "type" : ("int", 1)})
+            known_types.append({"name" : "BitCount",                         "type" : ("int", 32)})
+            known_types.append({"name" : "ConditionPassed",                  "type" : ("int", 1)})
+            known_types.append({"name" : "Coproc_Accepted",                  "type" : ("int", 1)})
+            known_types.append({"name" : "Coproc_DoneLoading",               "type" : ("int", 1)})
+            known_types.append({"name" : "Coproc_DoneStoring",               "type" : ("int", 1)})
+            known_types.append({"name" : "Coproc_GetOneWord",                "type" : ("int", 1)})
+            known_types.append({"name" : "Coproc_GetTwoWords",               "type" : ("list", 2)})
+            known_types.append({"name" : "Coproc_GetTwoWords",               "type" : ("list", 2)})
+            known_types.append({"name" : "Coproc_GetWordToStore",            "type" : ("int", 32)})
+            known_types.append({"name" : "CurrentCond",                      "type" : ("int", 4)})
+            known_types.append({"name" : "CurrentModeIsHyp",                 "type" : ("int", 1)})
+            known_types.append({"name" : "CurrentModeIsNotUser",             "type" : ("int", 1)})
+            known_types.append({"name" : "CurrentModeIsUserOrSystem",        "type" : ("int", 1)})
+            known_types.append({"name" : "DecodeImmShift",                   "type" : ("list", 2)})
+            known_types.append({"name" : "ELR_hyp",                          "type" : ("int", 32)})
+            known_types.append({"name" : "FPCompare",                        "type" : ("list", 4)})
+            known_types.append({"name" : "FPCompareEQ",                      "type" : ("int",  1)})
+            known_types.append({"name" : "FPCompareGE",                      "type" : ("int", 1)})
+            known_types.append({"name" : "FPCompareGT",                      "type" : ("int", 1)})
+            known_types.append({"name" : "FPDoubleToSingle",                 "type" : ("int", 32)})
+            known_types.append({"name" : "FPHalfToSingle",                   "type" : ("int", 32)})
+            known_types.append({"name" : "FPRSqrtEstimate",                  "type" : ("int", 32)})
+            known_types.append({"name" : "FPRSqrtStep",                      "type" : ("int", 32)})
+            known_types.append({"name" : "FPRecipEstimate",                  "type" : ("int", 32)})
+            known_types.append({"name" : "FPRecipStep",                      "type" : ("int", 32)})
+            known_types.append({"name" : "FPSingleToDouble",                 "type" : ("int", 64)})
+            known_types.append({"name" : "FPSingleToHalf",                   "type" : ("int", 16)})
+            known_types.append({"name" : "HasVirtExt",                       "type" : ("int", 1)})
+            known_types.append({"name" : "HaveLPAE",                         "type" : ("int", 1)})
+            known_types.append({"name" : "HaveMPExt",                        "type" : ("int", 1)})
+            known_types.append({"name" : "HaveVirtExt",                      "type" : ("int", 1)})
+            known_types.append({"name" : "InITBlock",                        "type" : ("int", 1)})
+            known_types.append({"name" : "IntegerZeroDivideTrappingEnabled", "type" : ("int", 1)})
+            known_types.append({"name" : "IsAlignmentFault",                 "type" : ("int", 1)})
+            known_types.append({"name" : "IsExternalAbort",                  "type" : ("int", 1)})
+            known_types.append({"name" : "IsExternalAbort",                  "type" : ("int", 1)})
+            known_types.append({"name" : "IsSecure",                         "type" : ("int", 1)})
+            known_types.append({"name" : "IsZero",                           "type" : ("int",  1)})
+            known_types.append({"name" : "IsZeroBit",                        "type" : ("int",  1)})
+            known_types.append({"name" : "JazelleAcceptsExecution",          "type" : ("int", 1)})
+            known_types.append({"name" : "LR",                               "type" : ("int", 32)})
+            known_types.append({"name" : "LSInstructionSyndrome",            "type" : ("int", 9)})
+            known_types.append({"name" : "LastInITBlock",                    "type" : ("int", 1)})
+            known_types.append({"name" : "PC",                               "type" : ("int", 32)})
+            known_types.append({"name" : "PCStoreValue",                     "type" : ("int", 32)})
+            known_types.append({"name" : "RemapRegsHaveResetValues",         "type" : ("int", 1)})
+            known_types.append({"name" : "SP",                               "type" : ("int", 32)})
+            known_types.append({"name" : "SatQ",                             "type" : ("list", 2)})
+            known_types.append({"name" : "Shift",                            "type" : ("int", 32)})
+            known_types.append({"name" : "Shift_C",                          "type" : ("list", 2)})
+            known_types.append({"name" : "SignedSatQ",                       "type" : ("list", 2)})
+            known_types.append({"name" : "StandardFPSCRValue",               "type" : ("int", 32)})
+            known_types.append({"name" : "ThisInstr",                        "type" : ("int", 32)})
+            known_types.append({"name" : "ThumbExpandImm",                   "type" : ("int", 32)})
+            known_types.append({"name" : "ThumbExpandImm_C",                 "type" : ("list", 2)})
+            known_types.append({"name" : "UnalignedSupport",                 "type" : ("int", 1)})
+            known_types.append({"name" : "UnsignedRSqrtEstimate",            "type" : ("int", 32)})
+            known_types.append({"name" : "UnsignedRecipEstimate",            "type" : ("int", 32)})
+            known_types.append({"name" : "UnsignedSatQ",                     "type" : ("list", 2)})
+
+            # I'm not sure if enums are 32 bits but probably yes.
+            known_types.append({"name" : "MBReqDomain_FullSystem", "type" : ("int", 32)})
+            known_types.append({"name" : "MBReqDomain_InnerShareable", "type" : ("int", 32)})
+            known_types.append({"name" : "MBReqDomain_Nonshareable", "type" : ("int", 32)})
+            known_types.append({"name" : "MBReqDomain_OuterShareable", "type" : ("int", 32)})
+            known_types.append({"name" : "MBReqTypes_All", "type" : ("int", 32)})
+            known_types.append({"name" : "MBReqTypes_Writes", "type" : ("int", 32)})
+
+            for symbol in symbols:
+                known_types.append({"name" : symbol, "type" : ("int", 32)})
+
+            # Translate the AST into code.
+            translator = ARMv7Parser.CPPTranslatorVisitor(known_types=known_types)
+
+            body = ""
+            for ast_node in program_ast:
+                ast_node = ast_node[0]
+                code = translator.accept(ast_node)
+                body += ARMv7Parser.indent(code)
+                
+                if type(ast_node) == ARMv7Parser.ProcedureCall:
+                    body = body[:-1] + ";\n"
+
+            # Write the translated body of the decoding procedure.
+            fd.write(body)            
             fd.write("    return true;\n")
             fd.write("};\n")
             fd.write("\n")
+
+            i += 1
             
     return True
 
@@ -111,6 +211,7 @@ def main():
 
     # Filenames and path's.
     gen_dir = os.path.abspath(args.gendir)
+    symbols_file = os.path.join(os.path.abspath("../../../libdisassembly/arm/gen/"), "symbols.sym")
     interpreter_name_h = os.path.join(gen_dir, "ARMInterpreter.h")
     interpreter_name_cpp = os.path.join(gen_dir, "ARMInterpreter.cpp")
 
@@ -130,8 +231,8 @@ def main():
 
         logging.info("Creating decoders at '%s'." % interpreter_name_h)
         logging.info("Creating decoders at '%s'." % interpreter_name_cpp)
-        
-        if not create_interpreter(interpreter_name_h, interpreter_name_cpp):
+
+        if not create_interpreter(interpreter_name_h, interpreter_name_cpp, symbols_file):
             logging.error("Could not create the interpreter.")
             return False
 
