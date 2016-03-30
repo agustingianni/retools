c485dacb66b9a01e81f1a548061badcfe8b9d23a
Agustin Gianni <agustingianni@gmail.com>
2016-03-31 01:39:19 +0200

    Removed unused functions.

--- src/utilities/debug.h
+++ src/utilities/debug.h
@@ -5,8 +5,6 @@
 #include <errno.h>
 #include <string.h>
 
-#define FILE (strrchr(__FILE__, '/') ? (const char *) strrchr(__FILE__, '/') + 1 : __FILE__)
-
 #ifdef NDEBUG
 #define LOG_DEBUG(M, ...)
 #else
@@ -14,19 +12,8 @@
 #endif
 
 #define CLEAN_ERRNO() (errno == 0 ? "None" : (const char *) strerror(errno))
-
 #define LOG_ERR(M, ...) fprintf(stderr, "[ERR] (errno: %s) " M "\n", CLEAN_ERRNO(), ##__VA_ARGS__)
-
 #define LOG_WARN(M, ...) fprintf(stderr, "[WRN] (errno: %s) " M "\n", CLEAN_ERRNO(), ##__VA_ARGS__)
-
 #define LOG_INFO(M, ...) fprintf(stderr, "[NFO] " M "\n", ##__VA_ARGS__)
-
-#define CHECK(A, M, ...) if(!(A)) { LOG_ERR(M, ##__VA_ARGS__); errno=0; goto error; }
-
-#define SENTINEL(M, ...)  { LOG_ERR(M, ##__VA_ARGS__); errno=0; goto error; }
-
-#define CHECK_MEM(A) CHECK((A), "Out of memory.")
-
-#define CHECK_DEBUG(A, M, ...) if(!(A)) { DEBUG(M, ##__VA_ARGS__); errno=0; goto error; }
 
 #endif
