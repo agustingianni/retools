bbf9080fe6213fa7783e4c8b3c1514d83dad99fe
Agustin Gianni <agustingianni@gmail.com>
2016-03-31 00:03:10 +0200

    Made all the methods const and added a method to check if a pointer falls within the managed memory.

--- src/utilities/MemoryMap.h
+++ src/utilities/MemoryMap.h
@@ -8,29 +8,38 @@
 #ifndef SRC_UTILITIES_MEMORYMAP_H_
 #define SRC_UTILITIES_MEMORYMAP_H_
 
+#include <cstdint>
+
 class MemoryMap {
 private:
-    unsigned char *m_memory;
-    unsigned char *m_end;
+    uint8_t *m_memory = nullptr;
+    uint8_t *m_end = nullptr;
 
 public:
-    MemoryMap(unsigned char *memory, size_t size) :
-            m_memory(memory), m_end(memory + size) {
+    MemoryMap() = default;
+
+    MemoryMap(uint8_t *memory, size_t size) :
+        m_memory(memory), m_end(memory + size) {
     }
 
     // Get a sane pointer to a given type and a memory pointer.
-    template<typename T> T *pointer(void *mem, size_t size = sizeof(T)) {
-        return valid(static_cast<unsigned char *>(mem) + size) ? reinterpret_cast<T *>(mem) : nullptr;
+    template<typename T> T *pointer(void *mem, size_t size = sizeof(T)) const {
+        return valid(static_cast<uint8_t *>(mem) + size) ? reinterpret_cast<T *>(mem) : nullptr;
     }
 
     // Get a sane pointer to a given type and an offset.
-    template<typename T> T *offset(unsigned offset, size_t size = sizeof(T)) {
+    template<typename T> T *offset(uint64_t offset, size_t size = sizeof(T)) const {
         return valid(m_memory + offset + size) ? reinterpret_cast<T *>(m_memory + offset) : nullptr;
     }
 
     // Validate a pointer.
-    bool valid(void *ptr) {
+    bool valid(void *ptr) const {
         return ptr <= m_end;
+    }
+
+    // Validate a pointer.
+    template<typename T> bool valid_pointer(T *ptr) const {
+        return reinterpret_cast<uint8_t *>((ptr + 1)) <= m_end;
     }
 };
 
