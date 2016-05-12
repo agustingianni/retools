f6a073b40785f196a1d4bc121e6f3946c4b72567
Agustin Gianni <agustingianni@gmail.com>
2016-05-13 01:51:53 +0200

    Added enums for each of the types of the registers in the ARM arch.

--- src/libdisassembly/arm/ARMArch.h
+++ src/libdisassembly/arm/ARMArch.h
@@ -275,6 +275,148 @@ typedef enum shift_t {
 	SRType_LSL, SRType_LSR, SRType_ASR, SRType_ROR, SRType_RRX, SRType_Invalid
 } shift_t;
 
+namespace Register {
+enum Core {
+    ARM_REG_R0 = 0,
+    ARM_REG_R1,
+    ARM_REG_R2,
+    ARM_REG_R3,
+    ARM_REG_R4,
+    ARM_REG_R5,
+    ARM_REG_R6,
+    ARM_REG_R7,
+    ARM_REG_R8,
+    ARM_REG_R9,
+    ARM_REG_R10,
+    ARM_REG_R11,
+    ARM_REG_R12,
+    ARM_REG_R13,
+    ARM_REG_R14,
+    ARM_REG_R15,
+    ARM_REG_CORE_MAX,
+    ARM_REG_SB = ARM_REG_R9,
+    ARM_REG_SL = ARM_REG_R10,
+    ARM_REG_FP = ARM_REG_R11,
+    ARM_REG_IP = ARM_REG_R12,
+    ARM_REG_SP = ARM_REG_R13,
+    ARM_REG_LR = ARM_REG_R14,
+    ARM_REG_PC = ARM_REG_R15
+};
+
+enum Coproc {
+    ARM_REG_CR0 = 0,
+    ARM_REG_CR1,
+    ARM_REG_CR2,
+    ARM_REG_CR3,
+    ARM_REG_CR4,
+    ARM_REG_CR5,
+    ARM_REG_CR6,
+    ARM_REG_CR7,
+    ARM_REG_CR8,
+    ARM_REG_CR9,
+    ARM_REG_CR10,
+    ARM_REG_CR11,
+    ARM_REG_CR12,
+    ARM_REG_CR13,
+    ARM_REG_CR14,
+    ARM_REG_CR15,
+    ARM_REG_COPROC_MAX
+};
+
+enum Single {
+    ARM_REG_S0 = 0,
+    ARM_REG_S1,
+    ARM_REG_S2,
+    ARM_REG_S3,
+    ARM_REG_S4,
+    ARM_REG_S5,
+    ARM_REG_S6,
+    ARM_REG_S7,
+    ARM_REG_S8,
+    ARM_REG_S9,
+    ARM_REG_S10,
+    ARM_REG_S11,
+    ARM_REG_S12,
+    ARM_REG_S13,
+    ARM_REG_S14,
+    ARM_REG_S15,
+    ARM_REG_S16,
+    ARM_REG_S17,
+    ARM_REG_S18,
+    ARM_REG_S19,
+    ARM_REG_S20,
+    ARM_REG_S21,
+    ARM_REG_S22,
+    ARM_REG_S23,
+    ARM_REG_S24,
+    ARM_REG_S25,
+    ARM_REG_S26,
+    ARM_REG_S27,
+    ARM_REG_S28,
+    ARM_REG_S29,
+    ARM_REG_S30,
+    ARM_REG_S31,
+    ARM_REG_SINGLE_MAX
+};
+
+enum Double {
+    ARM_REG_D0 = 0,
+    ARM_REG_D1,
+    ARM_REG_D2,
+    ARM_REG_D3,
+    ARM_REG_D4,
+    ARM_REG_D5,
+    ARM_REG_D6,
+    ARM_REG_D7,
+    ARM_REG_D8,
+    ARM_REG_D9,
+    ARM_REG_D10,
+    ARM_REG_D11,
+    ARM_REG_D12,
+    ARM_REG_D13,
+    ARM_REG_D14,
+    ARM_REG_D15,
+    ARM_REG_D16,
+    ARM_REG_D17,
+    ARM_REG_D18,
+    ARM_REG_D19,
+    ARM_REG_D20,
+    ARM_REG_D21,
+    ARM_REG_D22,
+    ARM_REG_D23,
+    ARM_REG_D24,
+    ARM_REG_D25,
+    ARM_REG_D26,
+    ARM_REG_D27,
+    ARM_REG_D28,
+    ARM_REG_D29,
+    ARM_REG_D30,
+    ARM_REG_D31,
+    ARM_REG_DOUBLE_MAX
+};
+
+enum Quad {
+    ARM_REG_Q0 = 0,
+    ARM_REG_Q1,
+    ARM_REG_Q2,
+    ARM_REG_Q3,
+    ARM_REG_Q4,
+    ARM_REG_Q5,
+    ARM_REG_Q6,
+    ARM_REG_Q7,
+    ARM_REG_Q8,
+    ARM_REG_Q9,
+    ARM_REG_Q10,
+    ARM_REG_Q11,
+    ARM_REG_Q12,
+    ARM_REG_Q13,
+    ARM_REG_Q14,
+    ARM_REG_Q15,
+    ARM_REG_QUAD_MAX
+};
+}
+
+// XXX: Remove this.
 typedef enum reg_t {
 	// Core registers.
 	r0, r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11,
