17fb23e7f8581f7635c84fdf357badc9544ea1fa
Agustin Gianni <agustingianni@gmail.com>
2016-06-06 14:10:07 +0200

    Initial implementation of ARMEmulator.cpp

--- src/libemulation/arm/ARMEmulator.cpp
+++ src/libemulation/arm/ARMEmulator.cpp
@@ -6,11 +6,50 @@
  */
 
 #include "ARMEmulator.h"
+#include "arm/ARMDisassembler.h"
+
+using namespace std;
+using namespace Register;
+using namespace Disassembler;
 
 namespace Emulator {
-    ARMEmulator::ARMEmulator() {
+    ARMEmulator::ARMEmulator(ARMContext &context, Memory::AbstractMemory &memory, ARMMode mode) :
+		m_mode{mode}, m_contex{context}, m_memory{memory} {
     }
 
     ARMEmulator::~ARMEmulator() {
+    }
+
+    void ARMEmulator::start(unsigned count) {
+        ARMDisassembler dis { ARMv7All };
+
+    	bool stop = false;
+    	unsigned n_executed = 0;
+
+    	uint32_t cur_pc = 0;
+    	uint32_t cur_opcode = 0;
+    	ARMMode cur_mode = m_mode;
+
+    	m_contex.getRegister(ARM_REG_PC, cur_pc);
+
+		while (!stop) {
+			if (n_executed == count) {
+				stop = true;
+				break;
+			}
+
+			// 1. Fetch an instruction from main memory.
+			m_memory.read_value(cur_pc, cur_opcode);
+
+			// 2. Decode it.
+			ARMInstruction ins = dis.disassemble(cur_opcode, cur_mode);
+		    printf("DEBUG: cur_pc=0x%.8x cur_opcode=0x%.8x string='%s' size=%d\n",
+		    		cur_pc, cur_opcode, ins.toString().c_str(), ins.size);
+
+			// 3. Execute the instruction.
+
+			cur_pc += sizeof(uint32_t);
+			n_executed++;
+		}
     }
 }
--- src/libemulation/arm/ARMEmulator.h
+++ src/libemulation/arm/ARMEmulator.h
@@ -8,12 +8,37 @@
 #ifndef SRC_LIBEMULATION_ARM_ARMEMULATOR_H_
 #define SRC_LIBEMULATION_ARM_ARMEMULATOR_H_
 
+#include "arm/ARMArch.h"
+#include "arm/ARMContext.h"
+#include "arm/gen/ARMInterpreter.h"
+#include "memory/Memory.h"
+
 namespace Emulator {
-    class ARMEmulator {
-    public:
-        ARMEmulator();
-        virtual ~ARMEmulator();
-    };
+	class ARMEmulator {
+	private:
+		ARMMode m_mode;
+		ARMContext &m_contex;
+		ARMInterpreter m_interpreter;
+		Memory::AbstractMemory &m_memory;
+
+	public:
+		ARMEmulator(ARMContext &context, Memory::AbstractMemory &memory, ARMMode mode = ARMMode_ARM);
+		virtual ~ARMEmulator();
+
+		void start(unsigned count = 0);
+
+		ARMContext &getContext() const {
+			return m_contex;
+		}
+
+		void setContext(ARMContext context) {
+			m_contex = context;
+		}
+
+		void setMode(ARMMode mode) {
+			m_mode = mode;
+		}
+	};
 }
 
 #endif /* SRC_LIBEMULATION_ARM_ARMEMULATOR_H_ */
