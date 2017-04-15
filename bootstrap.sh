34b2c2155959eb8b3129909327ba103ab1fbf1ed
Agustin Gianni <agustingianni@gmail.com>
2017-04-15 22:31:01 +0200

    Update bootstrap and cmake scripts
    Adds lldb as a dependency and does the right thing on linux

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,10 +8,18 @@ if [ ! -L retools ]; then
 fi
 
 # Install dependencies.
+UBUNTU_NAME=$(lsb_release -s -c)
+LLDB_VERSION="4.0"
+
+# We need lldb version 4.0 so we add llvm's custom apt repos.
+wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-add-repository "deb http://apt.llvm.org/$UBUNTU_NAME/ llvm-toolchain-$UBUNTU_NAME-$LLDB_VERSION main"
+
+# Update and install dependencies.
 sudo apt-get update
 sudo apt-get install build-essential git cmake python-setuptools \
     python-dev libboost-python-dev libglib2.0-dev pkg-config \
-    llvm clang python-pyparsing -y
+    llvm clang python-pyparsing liblldb-$LLDB_VERSION -y
 
 # Create a temporary directory.
 DEPS_DIR=$(mktemp -d)
