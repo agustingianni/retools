3ff0efb0d2ef472c7ea5bb4a5bb9962f9ebbb1da
Agustin Gianni <agustingianni@gmail.com>
2017-08-09 12:20:24 +0200

    Darms lib name is not important, leave it as is

--- third_party/darm/install.sh
+++ third_party/darm/install.sh
@@ -6,10 +6,5 @@ pushd $BINARY_DIR > /dev/null
 cp libdarm.a $INSTALL_DIR/usr/local/lib
 cp libdarm.so $INSTALL_DIR/usr/local/lib
 
-# Darm's makefile sucks.
-if [ "$(uname)" == "Darwin" ] ; then
-    mv $INSTALL_DIR/usr/local/lib/libdarm.so $INSTALL_DIR/usr/local/lib/libdarm.dylib
-fi
-
 cp *.h $INSTALL_DIR/usr/local/include
 popd > /dev/null
