efaaebc046b488e0c224a84c8d78a3e75593f6b9
Agustin Gianni <agustingianni@gmail.com>
2017-08-13 23:03:27 +0200

    Fixes installation of libdarm on osx

--- third_party/darm/install.sh
+++ third_party/darm/install.sh
@@ -4,7 +4,13 @@ INSTALL_DIR=$2
 
 pushd $BINARY_DIR > /dev/null
 cp libdarm.a $INSTALL_DIR/usr/local/lib
-cp libdarm.so $INSTALL_DIR/usr/local/lib
+
+UNAME=$(uname)
+if [ "$UNAME" == "Linux" ] ; then
+    cp libdarm.so $INSTALL_DIR/usr/local/lib/libdarm.so
+elif [ "$UNAME" == "Darwin" ] ; then
+    cp libdarm.so $INSTALL_DIR/usr/local/lib/libdarm.dylib
+fi
 
 cp *.h $INSTALL_DIR/usr/local/include
 popd > /dev/null
